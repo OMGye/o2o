64c4ba184227b04c43cef453251f92854b3fabe8
OMGye <1252078414@qq.com>
2018-12-10 15:40:49 +0800

    update

--- src/main/java/com/util/MailUtil.java
+++ src/main/java/com/util/MailUtil.java
@@ -21,6 +21,11 @@ public class MailUtil {
 
 		props.setProperty("mail.host", "smtp.163.com");
 
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.socketFactory.fallback", "false");
+		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.socketFactory.port", "465");
+
 		props.setProperty("mail.smtp.auth", "true");
 
 
